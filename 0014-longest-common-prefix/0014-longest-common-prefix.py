88e523cff2f3970164a060b6f95bdd3a4dde03d1
Samarth Gumaste <samgumaste2008@gmail.com>
2026-09-05 14:55:34 +0530

    Time: 0 ms (100%), Space: 19.2 MB (73.62%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -3,7 +3,7 @@ class Solution:
         pref = strs[0]
         pref_len = len(pref)
 
-        for s in strs[1:]:
+        for s in strs[1:len(strs)]:
             while pref != s[0:pref_len]:
                 pref_len -= 1
                 if pref_len == 0:
